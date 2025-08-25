0c35522a64d395540aceee9f8a92619705df1745
BlissX-ML <liumotong95@gmail.com>
2025-08-25 21:30:30 +0800

    Deploy in Github, delete baseName setting and use createHashRouter.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import svgr from 'vite-plugin-svgr';
 import mdx from '@mdx-js/rollup'
 import remarkGfm from 'remark-gfm'
 
-const isProd = process.env.NODE_ENV === 'production'
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: isProd ? '/Soluna/' : '/',
   plugins: [
     react(),
     svgr(),
